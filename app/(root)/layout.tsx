65d700f2d442c6f9b015880c4530f6dbfeca82ea
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-09-09 00:20:03 -0300

    Finished the Server Actions, Models, uploadThing, added Forms, and Shared Components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,12 +2,16 @@ import { ClerkProvider } from "@clerk/nextjs";
 import { Inter } from "next/font/google";
 
 import "../globals.css";
+import TopBar from "@/components/shared/TopBar";
+import LeftSideBar from "@/components/shared/LeftSideBar";
+import RightSideBar from "@/components/shared/RightSideBar";
+import BottomBar from "@/components/shared/BottomBar";
 
 export const metadata = {
   title: "Threads",
   description: "Next.js 13 Meta Threads Application",
 };
-
+<link rel="icon" href="/icon.ico" sizes="any" />;
 const inter = Inter({ subsets: ["latin"] });
 
 export default function RootLayout({
@@ -18,7 +22,17 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="pt-BR">
-        <body className={`${inter.className} bg-dark-1`}>{children}</body>
+        <body className={inter.className}>
+          <TopBar />
+          <main className="flex flex-row">
+            <LeftSideBar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            <RightSideBar />
+          </main>
+          <BottomBar />
+        </body>
       </html>
     </ClerkProvider>
   );
