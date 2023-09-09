8149e649588a0e0da0438bb3e763bca6d34bad44
Claudenir Nojosa <clau.nojosaf@gmail.com>
2023-09-09 00:35:02 -0300

    Creating the Create Post funcionality

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -5,7 +5,7 @@ import User from "../models/user.model";
 import { connectToDB } from "../mongoose";
 
 interface Params {
-  text: String;
+  text: string;
   author: string;
   communityId: string | null;
   path: string;
@@ -33,4 +33,51 @@ export async function createThread({
   } catch (error: any) {
     throw new Error(`Error creating Thread: ${error.message}`);
   }
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+  connectToDB();
+
+  // Calculate the number of posts to skip based on the page number and page size.
+  const skipAmount = (pageNumber - 1) * pageSize;
+
+  // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+  const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+    .sort({ createdAt: "desc" })
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({
+      path: "author",
+      model: User,
+    })
+    .populate({
+      path: "community",
+      model: Community,
+    })
+    .populate({
+      path: "children", // Populate the children field
+      populate: {
+        path: "author", // Populate the author field within children
+        model: User,
+        select: "_id name parentId image", // Select only _id and username fields of the author
+      },
+    });
+
+  // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+  const totalPostsCount = await Thread.countDocuments({
+    parentId: { $in: [null, undefined] },
+  }); // Get the total count of posts
+
+  const posts = await postsQuery.exec();
+
+  const isNext = totalPostsCount > skipAmount + posts.length;
+
+  return { posts, isNext };
+}
+
+interface Params {
+  text: string;
+  author: string;
+  communityId: string | null;
+  path: string;
 }
